f3e15f6a95e8e7fdb2da1fb61578e1bd8d380fb8
martinfield <madeinarcade@gmail.com>
2022-09-26 17:58:44 -0700

    created searchBar component and slice

---
+++ src/features/SearchBar/searchbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function SearchBar() {
+    return (
+        <input 
+        type='search'
+        placeholder="Search Reddit..."
+        >
+            
+        </input>
+    )
+}

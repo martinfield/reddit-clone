323723fc28a4a8ae80bf0f07ca3ca1540cdb80c5
martinfield <madeinarcade@gmail.com>
2022-09-26 17:56:38 -0700

    created homepageSlice with thunks and reducers

---
+++ src/features/HomePage/homepageSlice.js
@@ -0,0 +1,110 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API_ROOT } from "../../api/reddit";
+
+export const loadBestPage = createAsyncThunk(
+    "homepage/loadBestPage",
+    async(thunkAPI) => {
+        const data = await fetch(`http://www.reddit.com/best.json`);
+        const json = await data.json();
+
+        return json;
+    }
+)
+
+export const loadHotPage = createAsyncThunk(
+    "homepage/loadHotPage",
+    async(thunkAPI) => {
+        const data = await fetch(`${API_ROOT}/hot.json`);
+        const json = await data.json();
+
+        return json;
+    }
+)
+
+export const loadNewPage = createAsyncThunk(
+    "homepage/loadNewPage",
+    async(thunkAPI) => {
+        const data = await fetch(`${API_ROOT}/new.json`);
+        const json = await data.json();
+
+        return json;
+    }
+)
+
+export const loadTopPage = createAsyncThunk(
+    "homepage/loadTopPage",
+    async(thunkAPI) => {
+        const data = await fetch(`${API_ROOT}/top.json`);
+        const json = await data.json();
+
+        return json;
+    }
+)
+
+export const homePageSlice = createSlice({
+    name: 'homePage',
+    initialState: {
+        posts: [],
+        isLoading: false,
+        hasError: false
+    },
+    reducers: {
+     
+    },
+    extraReducers: {
+        [loadBestPage.pending]: (state, action) => {
+            state.isLoading = true;
+            state.hasError = false;
+        },
+        [loadBestPage.fulfilled]: (state, action) => {
+            state.posts = action.payload;
+            state.isLoading = false;
+            state.hasError = false;
+        },
+        [loadBestPage.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.hasError = true;
+        },
+        [loadHotPage.pending]: (state, action) => {
+            state.isLoading = true;
+            state.hasError = false;
+        },
+        [loadHotPage.fulfilled]: (state, action) => {
+            state.posts = action.payload;
+            state.isLoading = false;
+            state.hasError = false;
+        },
+        [loadHotPage.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.hasError = true;
+        },
+        [loadNewPage.pending]: (state, action) => {
+            state.isLoading = true;
+            state.hasError = false;
+        },
+        [loadNewPage.fulfilled]: (state, action) => {
+            state.posts = action.payload;
+            state.isLoading = false;
+            state.hasError = false;
+        },
+        [loadNewPage.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.hasError = true;
+        },
+        [loadTopPage.pending]: (state, action) => {
+            state.isLoading = true;
+            state.hasError = false;
+        },
+        [loadTopPage.fulfilled]: (state, action) => {
+            state.posts = action.payload;
+            state.isLoading = false;
+            state.hasError = false;
+        },
+        [loadTopPage.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.hasError = true;
+        }
+    }
+})
+
+export default homePageSlice.reducer

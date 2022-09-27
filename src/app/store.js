cb20b50bb577bc3e61b752f992862d3ed40ae842
martinfield <madeinarcade@gmail.com>
2022-09-26 17:54:21 -0700

    added homePage reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import homepageSliceReducer from '../features/HomePage/homepageSlice'; 
 
 
 export const store = configureStore({
   reducer: {
-    
+    homePage: homepageSliceReducer
   },
 });

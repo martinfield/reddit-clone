e868128654ca9af21a090dce3a93b2bf02580ea6
martinfield <madeinarcade@gmail.com>
2022-10-11 10:50:47 -0700

    map through best posts, assigning data as props

--- src/features/HomePage/Best/best.js
+++ src/features/HomePage/Best/best.js
@@ -1,17 +1,38 @@
-import React, {useEffect} from "react";
-import { useDispatch } from "react-redux";
-import { loadBestPage } from "../homepageSlice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadBestPage, selectPosts} from "../homepageSlice";
+import { Post } from "../../Post/post";
+
 
 export function Best() {
     const dispatch = useDispatch();
-    
-    useEffect(()=> {
+    const bestPosts = useSelector(selectPosts);
+
+    useEffect(() => {
         dispatch(loadBestPage());
     }, [dispatch])
 
+
         return (
-            <div>
-                Now showing Page: best
-            </div>
+        <div> 
+            {bestPosts.map((post)=> {
+                return (
+                <Post
+                key={post.id}
+                post={post}
+                title={post.title}
+                author={`u/${post.author}`}
+                subredditPrefixed={post['subreddit_name_prefixed']}
+                subreddit={post.subreddit}
+                url={post.url}
+                media={post.media}
+                comments={post['num_comments']}
+                votes={post.ups}
+                posthint={post['post_hint']}
+                />
+            )})}
+        </div>
         )
-    }
+       
+}
+
